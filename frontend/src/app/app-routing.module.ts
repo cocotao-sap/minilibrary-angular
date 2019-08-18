dc9df4d8069773f2083e82d020974b10e48a145b
cocotao <coco.tao@sap.com>
2019-08-18 22:37:18 +0800

    update

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {WechatAuthorizationComponent} from './wechat-authorization/wechat-authorization.component'
+import { WechatAuthorizationComponent } from './wechat-authorization/wechat-authorization.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginPageComponent },
   { path: 'wechatAuthorization', component: WechatAuthorizationComponent }
 ];
 
